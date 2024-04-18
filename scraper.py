b6b1d734582bd951c8dc6d9c57dc0ea095786e55
EffPeeEl <65282480+EffPeeEl@users.noreply.github.com>
2024-04-18 18:41:20 +0200

    fixed deleted

--- scraper.py
+++ scraper.py
@@ -1,31 +1,33 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-driver = webdriver.Chrome(ChromeDriverManager().install())
-all_nation_url = {
-"snerikes" : "https://www.facebook.com/snerikes/events",
-"stockholm" : "https://www.facebook.com/stockholmsnation/events"
 
-#add all
-}
-
-x = 0
-try:
-    for key, url in all_nation_url.items(): 
-        driver.get(url)
-        time.sleep(10)
-
-
-        print("Page Title:", driver.title)
-
-        html_content = driver.page_source
-
-        with open(f'page_sources/{key}_page_source.html', 'w', encoding='utf-8') as f:
-            f.write(html_content)
-        x+=1
-
-
-finally:
-    driver.quit()
-
-print(f"HTML content saved, {x} items")
+def scrape():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    all_nation_url = {
+    "snerikes" : "https://www.facebook.com/snerikes/events",
+    "stockholm" : "https://www.facebook.com/stockholmsnation/events"
+    
+    #add all
+    }
+    
+    x = 0
+    try:
+        for key, url in all_nation_url.items(): 
+            driver.get(url)
+            time.sleep(10)
+    
+    
+            print("Page Title:", driver.title)
+    
+            html_content = driver.page_source
+    
+            with open(f'page_sources/{key}_page_source.html', 'w', encoding='utf-8') as f:
+                f.write(html_content)
+            x+=1
+    
+    
+    finally:
+        driver.quit()
+    
+    print(f"HTML content saved, {x} items")
