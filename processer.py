15c7346ff9738c1590f094f05c56630b6ed5830a
EffPeeEl <65282480+EffPeeEl@users.noreply.github.com>
2024-04-18 19:18:16 +0200

    fixes

--- processer.py
+++ processer.py
@@ -6,10 +6,11 @@ def process_html():
     save_file = "events.json"
     
     directory_path = os.path.join(os.getcwd(), "page_sources")
+    print(directory_path)
     if not os.path.exists(directory_path):
         exit()
     
-        
+    events = []
     for file_loc in os.listdir(directory_path):
         print(file_loc)
     
@@ -24,7 +25,7 @@ def process_html():
         event_blocks = soup.find_all('div', class_="x6s0dn4 x1lq5wgf xgqcy7u x30kzoy x9jhf4c x1olyfxc x9f619 x78zum5 x1e56ztr xyamay9 x1pi30zi x1l90r2v x1swvt13 x1gefphp")
         print()
     
-        events = []
+        
         for event in event_blocks:
         
             title_link = event.find('a', class_="x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g xt0b8zv x1s688f")
@@ -34,6 +35,7 @@ def process_html():
     
             event_info = {
                 "title": title_link.text,
+                "nation" : file_loc.split("_")[0],
                 "url": title_link['href'],
                 "image_url": image_tag['src'],
                 "time": time_span.text.strip(),
@@ -41,11 +43,11 @@ def process_html():
             }
             events.append(event_info)
     
-        with open(save_file, 'w') as f:
-            json.dump(events, f, indent=4)
-            
-        print(f"Dumped: {len(events)} events from {file_loc} to {save_file}")
+    with open(save_file, 'w') as f:
+        json.dump(events, f, indent=4)
+        
+    print(f"Dumped: {len(events)} events to {save_file}")
     
     
     
-    print(f"Events extracted and saved to '{file_loc}'")
+    print(f"Events extracted and saved to '{save_file}'")
