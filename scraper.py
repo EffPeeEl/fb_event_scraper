91f069d1fcc6abb9f0a8a769dbbe25663d97ab68
EffPeeEl <65282480+EffPeeEl@users.noreply.github.com>
2024-04-19 10:55:37 +0200

    yes

--- scraper.py
+++ scraper.py
@@ -8,26 +8,27 @@ def run_scraping(save_location="page_sources",
 
     ):
 
-    all_nation_url = urls
     driver = webdriver.Chrome(ChromeDriverManager().install())
+    all_nation_url = {
+    "snerikes" : "https://www.facebook.com/snerikes/events",
+    "stockholm" : "https://www.facebook.com/stockholmsnation/events"
+
+    #add all
+    }
 
     x = 0
     try:
         for key, url in all_nation_url.items(): 
             driver.get(url)
-            time.sleep(10)
+            time.sleep(5)
 
 
             print("Page Title:", driver.title)
-
             html_content = driver.page_source
 
-            with open(f'{save_location}/{key}_page_source.html', 'w', encoding='utf-8') as f:
+            with open(f'page_sources/{key}_page_source.html', 'w', encoding='utf-8') as f:
                 f.write(html_content)
             x+=1
-
-
     finally:
         driver.quit()
-
     print(f"HTML content saved, {x} items")
